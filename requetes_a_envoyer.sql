0483dcafd148f1e84a2f66c592d7637814d095b7
Papaschultzgmod <alexconstantin52@hotmail.fr>
2021-08-31 11:51:18 +0200

    Create requetes_a_envoyer.sql

---
+++ requetes_a_envoyer.sql
@@ -0,0 +1,5 @@
+INSERT INTO particulier (NumParticulier, NomParticulier, PrenomParticulier, AutresPrenomsParticulier)
+VALUES ('1', 'Durand', 'Nicolas', 'Alexis');
+
+INSERT INTO entreprise (NomEntreprise, NumSIRET, NumTVA, NumInscriptionEntreprise)
+VALUES ('Decomep', '945154525165484', '64262', '894485165');
